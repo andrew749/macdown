09faa3baf6a47482a879897d2458785f77e6cc87
Andrew Codispoti <andrewcod749@gmail.com>
2017-04-11 19:37:10 -0400

    Added all keycodes. Cleaned up a bit. Added some documentation.

--- MacDown/Code/Utility/VimModeHelper.swift
+++ MacDown/Code/Utility/VimModeHelper.swift
@@ -17,8 +17,8 @@ import Carbon
     let matcher: Matcher<(NSTextView) ->()>
     
     // logical codes we use in our own mappings
+    // exists so we can easily remap in the future
     enum KEYCODE {
-        case ESCAPE_KEY
         case A_CODE
         case B_CODE
         case C_CODE
@@ -45,11 +45,23 @@ import Carbon
         case X_CODE
         case Y_CODE
         case Z_CODE
+        
+        case ZERO_CODE
+        case ONE_CODE
+        case TWO_CODE
+        case THREE_CODE
+        case FOUR_CODE
+        case FIVE_CODE
+        case SIX_CODE
+        case SEVEN_CODE
+        case EIGHT_CODE
+        case NINE_CODE
+        
+        case ESCAPE_KEY
         case SHIFT_CODE
         case COMMAND_CODE
         case CONTROL_CODE
         case OPTION_CODE
-        case ZERO_CODE
         case DOLLAR_SIGN_CODE
         case NOT_SPECIAL
     }
@@ -102,46 +114,79 @@ import Carbon
     override init() {
         
         symToKeyCode = [
-            kVK_ANSI_I: KEYCODE.I_CODE,
-            kVK_ANSI_H: KEYCODE.H_CODE,
-            kVK_ANSI_J: KEYCODE.J_CODE,
-            kVK_ANSI_L: KEYCODE.L_CODE,
-            kVK_ANSI_K: KEYCODE.K_CODE,
-            kVK_ANSI_O: KEYCODE.O_CODE,
-            kVK_ANSI_G: KEYCODE.G_CODE,
-            kVK_ANSI_0: KEYCODE.ZERO_CODE,
-            kVK_ANSI_W: KEYCODE.W_CODE,
-            kVK_ANSI_B: KEYCODE.B_CODE,
-            kVK_ANSI_D: KEYCODE.D_CODE,
-            kVK_ANSI_X: KEYCODE.X_CODE,
             kVK_ANSI_A: KEYCODE.A_CODE,
-            kVK_ANSI_U: KEYCODE.U_CODE,
+            kVK_ANSI_B: KEYCODE.B_CODE,
+            kVK_ANSI_C: KEYCODE.C_CODE,
+            kVK_ANSI_D: KEYCODE.D_CODE,
+            kVK_ANSI_E: KEYCODE.E_CODE,
+            kVK_ANSI_F: KEYCODE.F_CODE,
+            kVK_ANSI_G: KEYCODE.G_CODE,
+            kVK_ANSI_H: KEYCODE.H_CODE,
+            kVK_ANSI_I: KEYCODE.I_CODE,
+            kVK_ANSI_J: KEYCODE.J_CODE,
+            kVK_ANSI_K: KEYCODE.K_CODE,
+            kVK_ANSI_L: KEYCODE.L_CODE,
+            kVK_ANSI_M: KEYCODE.M_CODE,
+            kVK_ANSI_N: KEYCODE.N_CODE,
+            kVK_ANSI_O: KEYCODE.O_CODE,
+            kVK_ANSI_P: KEYCODE.P_CODE,
+            kVK_ANSI_Q: KEYCODE.Q_CODE,
             kVK_ANSI_R: KEYCODE.R_CODE,
+            kVK_ANSI_S: KEYCODE.S_CODE,
+            kVK_ANSI_T: KEYCODE.U_CODE,
+            kVK_ANSI_U: KEYCODE.U_CODE,
+            kVK_ANSI_V: KEYCODE.V_CODE,
+            kVK_ANSI_W: KEYCODE.W_CODE,
+            kVK_ANSI_X: KEYCODE.X_CODE,
+            kVK_ANSI_Y: KEYCODE.Y_CODE,
+            kVK_ANSI_Z: KEYCODE.Z_CODE,
+            
+            kVK_ANSI_0: KEYCODE.ZERO_CODE,
+            kVK_ANSI_1: KEYCODE.ONE_CODE,
+            kVK_ANSI_2: KEYCODE.TWO_CODE,
+            kVK_ANSI_3: KEYCODE.THREE_CODE,
+            kVK_ANSI_4: KEYCODE.FOUR_CODE,
+            kVK_ANSI_5: KEYCODE.FIVE_CODE,
+            kVK_ANSI_6: KEYCODE.SIX_CODE,
+            kVK_ANSI_7: KEYCODE.SEVEN_CODE,
+            kVK_ANSI_8: KEYCODE.EIGHT_CODE,
+            kVK_ANSI_9: KEYCODE.NINE_CODE,
+            
+            kVK_Escape: KEYCODE.ESCAPE_KEY,
+            
+            // be careful, seems like these codes are just handled as flags in MacOS
+            kVK_Control: KEYCODE.CONTROL_CODE,
             kVK_Shift: KEYCODE.SHIFT_CODE,
             kVK_RightShift: KEYCODE.SHIFT_CODE,
-            kVK_Escape: KEYCODE.ESCAPE_KEY
+            kVK_Command: KEYCODE.COMMAND_CODE,
+            kVK_Option: KEYCODE.OPTION_CODE,
         ]
         
         matcher = Matcher()
         
         // commands that we have so far
+        // navigation commands
         matcher.register(commands:[KEYCODE.H_CODE] , action: VimModeHelper.moveLeft)
         matcher.register(commands: [KEYCODE.J_CODE], action: VimModeHelper.moveDown)
         matcher.register(commands: [KEYCODE.K_CODE], action: VimModeHelper.moveUp)
         matcher.register(commands: [KEYCODE.L_CODE], action: VimModeHelper.moveRight)
-        matcher.register(commands: [KEYCODE.O_CODE], action: VimModeHelper.newLineBelow)
+        matcher.register(commands: [KEYCODE.CONTROL_CODE, KEYCODE.D_CODE], action:VimModeHelper.moveForwardFractionPage)
+        matcher.register(commands: [KEYCODE.CONTROL_CODE, KEYCODE.U_CODE], action: VimModeHelper.moveBackwardFractionPage)
+        matcher.register(commands: [KEYCODE.ZERO_CODE], action: VimModeHelper.moveToBeginningOfLine)
+        matcher.register(commands: [KEYCODE.SHIFT_CODE, KEYCODE.G_CODE], action: VimModeHelper.endOfPage)
+        matcher.register(commands: [KEYCODE.G_CODE, KEYCODE.G_CODE], action: VimModeHelper.topOfPage)
         matcher.register(commands: [KEYCODE.W_CODE], action: VimModeHelper.moveForwardWord)
         matcher.register(commands: [KEYCODE.B_CODE], action: VimModeHelper.moveBackwardWord)
-        matcher.register(commands: [KEYCODE.ZERO_CODE], action: VimModeHelper.moveToBeginningOfLine)
-        matcher.register(commands: [KEYCODE.G_CODE, KEYCODE.G_CODE], action: VimModeHelper.topOfPage)
-        matcher.register(commands: [KEYCODE.SHIFT_CODE, KEYCODE.G_CODE], action: VimModeHelper.endOfPage)
+        
+        // commands to make modifications
+        matcher.register(commands: [KEYCODE.O_CODE], action: VimModeHelper.newLineBelow)
         matcher.register(commands: [KEYCODE.D_CODE, KEYCODE.D_CODE], action: VimModeHelper.deleteLine)
         matcher.register(commands: [KEYCODE.X_CODE], action: VimModeHelper.deleteUnderMouse)
         matcher.register(commands: [KEYCODE.A_CODE], action: VimModeHelper.appendCommand)
+        
+        // redo undo
         matcher.register(commands: [KEYCODE.U_CODE], action: VimModeHelper.undo)
         matcher.register(commands: [KEYCODE.CONTROL_CODE, KEYCODE.R_CODE], action: VimModeHelper.redo)
-        matcher.register(commands: [KEYCODE.CONTROL_CODE, KEYCODE.D_CODE], action:VimModeHelper.moveForwardFractionPage)
-        matcher.register(commands: [KEYCODE.CONTROL_CODE, KEYCODE.U_CODE], action: VimModeHelper.moveBackwardFractionPage)
         
         super.init()
     }
@@ -302,4 +347,5 @@ import Carbon
     {
         t.undoManager?.redo()
     }
+    
 }
